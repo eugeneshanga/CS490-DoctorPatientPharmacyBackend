39c88f5b48f2707a94b5fc3f6f9e1a517970426b
Eugene Shanga <es472@njit.edu>
2025-04-16 22:42:39 -0400

    resolved merge conflict with app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  # Allow cross-origin requests
-from Blueprints.pharmacyDashboard import pharmacist_dashboard_bp
+from flask_cors import CORS
+
+from blueprints.pharmacyDashboard.prescriptions import pharmacy_prescriptions_bp
+from blueprints.pharmacyDashboard.patients import pharmacy_patients_bp
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS so that your React app can call this API
+CORS(app)
 
-app.register_blueprint(pharmacist_dashboard_bp)
+app.register_blueprint(pharmacy_prescriptions_bp)
+app.register_blueprint(pharmacy_patients_bp)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello World!")
 
 if __name__ == '__main__':
+    print(app.url_map)
     app.run(debug=True)
