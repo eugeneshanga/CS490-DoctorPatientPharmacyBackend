c545341594269697905fdc061d923a2f93cc9c3d
nickstern2002 <nicksternheim@gmail.com>
2025-04-27 20:17:49 -0400

    feat: setup more payments stuff

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from blueprints.serviceDoctor.submitPrescription import prescriptions_bp
 from blueprints.prescriptionQueue.queue import pharmacy_queue_bp
 from blueprints.drugPrices.prices import prices_bp
 from blueprints.dispensePrescription.dispense import dispense_prescription_bp
+from blueprints.paymentHistory.payments import payments_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +18,7 @@ app.register_blueprint(prescriptions_bp)
 app.register_blueprint(pharmacy_queue_bp)
 app.register_blueprint(prices_bp)
 app.register_blueprint(dispense_prescription_bp)
+app.register_blueprint(payments_bp)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
